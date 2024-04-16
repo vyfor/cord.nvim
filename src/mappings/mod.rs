06dfb1f3a45b7623916ecd79c2ee0563544e2bd0
ReBlast <reblastoff@gmail.com>
2024-04-16 17:33:15 +0500

    refactor: provide a single function to fetch mappings and an enum to differentiate between filetypes

--- src/mappings/mod.rs
+++ src/mappings/mod.rs
@@ -1,3 +1,35 @@
 pub mod file_browser;
 pub mod language;
 pub mod plugin_manager;
+
+pub fn get_by_filetype<'a>(
+    filetype: &'a str,
+    filename: &'a str,
+) -> Filetype<'a> {
+    language::get(filetype, filename)
+        .map(|language| Filetype::Language(language.0, language.1))
+        .unwrap_or_else(|| {
+            file_browser::get(filetype)
+                .map(|file_browser| {
+                    Filetype::FileBrowser(file_browser.0, file_browser.1)
+                })
+                .unwrap_or_else(|| {
+                    plugin_manager::get(filetype)
+                        .map(|plugin_manager| {
+                            Filetype::PluginManager(
+                                plugin_manager.0,
+                                plugin_manager.1,
+                            )
+                        })
+                        .unwrap_or_else(|| {
+                            Filetype::Language("default", filetype)
+                        })
+                })
+        })
+}
+
+pub enum Filetype<'a> {
+    Language(&'a str, &'a str),
+    FileBrowser(&'a str, &'a str),
+    PluginManager(&'a str, &'a str),
+}
