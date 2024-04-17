8529e6fc03c2bcfb6d41832d9837cc90317af92d
ReBlast <reblastoff@gmail.com>
2024-04-17 21:22:48 +0500

    feat: add support for more file icons

--- src/mappings/language.rs
+++ src/mappings/language.rs
@@ -2,35 +2,72 @@ pub fn get<'a>(
     filetype: &'a str,
     filename: &str,
 ) -> Option<(&'a str, &'a str)> {
+    let filename = filename.to_lowercase();
     let language = match filetype {
+        "autohotkey" => ("ahk", "AutoHotkey"),
         "asm" => ("assembly", "Assembly"),
         "bash" => ("shell", "Bash"),
         "c" => ("c", "C "),
+        "clojure" => ("clojure", "Clojure"),
         "cpp" => ("cpp", "C++"),
+        "cr" => ("crystal", "Crystal"),
         "cs" => ("csharp", "C#"),
         "css" => ("css", "CSS"),
+        "d" => ("d", "D "),
         "dart" => ("dart", "Dart"),
         "dosbatch" => ("shell", "Batch"),
+        "elixir" => ("elixir", "Elixir"),
+        "erlang" => ("erlang", "Erlang"),
+        "fsharp" => ("fsharp", "F#"),
+        "git" | "gitignore" => ("git", "Git"),
         "go" => ("go", "Go"),
+        "groovy" => {
+            if filename == "build.gradle" {
+                ("gradle", "Gradle")
+            } else {
+                ("groovy", "Groovy")
+            }
+        }
         "haskell" => ("haskell", "Haskell"),
         "html" => ("html", "HTML"),
         "java" => ("java", "Java"),
         "javascript" => ("javascript", "JavaScript"),
         "javascriptreact" => ("react", "JSX"),
         "json" => ("json", "JSON"),
-        "kotlin" => ("kotlin", "Kotlin"),
+        "kotlin" => {
+            if filename == "build.gradle.kts" {
+                ("gradle", "Gradle")
+            } else {
+                ("kotlin", "Kotlin")
+            }
+        }
+        "tex" | "texmf" => ("latex", "LaTeX"),
+        "lisp" => ("lisp", "Lisp"),
         "lua" => ("lua", "Lua"),
         "markdown" => ("markdown", "Markdown"),
+        "nim" => ("nim", "Nim"),
+        "nix" => ("nix", "Nix"),
+        "ocaml" => ("ocaml", "OCaml"),
+        "pascal" => ("pascal", "Pascal"),
         "perl" => ("perl", "Perl"),
         "php" => ("php", "PHP"),
         "ps1" => ("powershell", "PowerShell"),
         "python" => ("python", "Python"),
+        "r" => ("r", "R "),
         "ruby" => ("ruby", "Ruby"),
         "rust" => ("rust", "Rust"),
         "scala" => ("scala", "Scala"),
+        "sass" | "scss" => ("scss", "Sass"),
         "sql" => ("sql", "SQL"),
+        "svelte" => ("svelte", "Svelte"),
         "swift" => ("swift", "Swift"),
-        "txt" => ("text", "Plain Text"),
+        "txt" => {
+            if filename == "license" {
+                ("license", "License")
+            } else {
+                ("text", "Plain Text")
+            }
+        }
         "toml" => {
             if filename == "Cargo.toml" {
                 ("cargo", "Cargo")
@@ -40,10 +77,20 @@ pub fn get<'a>(
         }
         "typescript" => ("typescript", "TypeScript"),
         "typescriptreact" => ("react", "TSX"),
+        "v" => ("v", "V "),
         "vim" => ("vim", "VimL"),
+        "vue" => ("vue", "Vue"),
         "xml" => ("xml", "XML"),
         "yaml" => ("yaml", "YAML"),
-        _ => return None,
+        "zig" => ("zig", "Zig"),
+        _ => match filename.rsplit_once('.') {
+            Some((_, extension)) => match extension {
+                "gml" => ("gml", "Game Maker Language"),
+                "pcss" | "postcss" => ("postcss", "PostCSS"),
+                _ => return None,
+            },
+            None => return None,
+        },
     };
 
     Some(language)
