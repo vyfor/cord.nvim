51360343abd4ebe9fa7636601353e28f24ae46e2
ReBlast <reblastoff@gmail.com>
2024-05-03 17:32:37 +0500

    refactor: rename `LSP Config` -> `lspconfig`

--- src/mappings/lsp_manager.rs
+++ src/mappings/lsp_manager.rs
@@ -1,6 +1,6 @@
 pub fn get<'a>(filetype: &'a str) -> Option<(&'a str, &'a str)> {
     let lsp_manager = match filetype {
-        "lspinfo" => ("default", "LSP Config"),
+        "lspinfo" => ("default", "lspconfig"),
         "mason" => ("default", "Mason"),
         _ => return None,
     };
