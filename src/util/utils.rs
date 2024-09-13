1dbd377360f51130539081eb0aa07cc6c542c9f2
ReBlast <reblastoff@gmail.com>
2024-09-13 17:03:44 +0500

    chore: bump assets version

--- src/util/utils.rs
+++ src/util/utils.rs
@@ -19,7 +19,7 @@ use crate::{
 pub const GITHUB_ASSETS_URL: &str =
     "http://raw.githubusercontent.com/vyfor/cord.nvim/master/assets";
 // Increment when modifying an existing icon
-const ASSETS_VERSION: &str = "14";
+const ASSETS_VERSION: &str = "15";
 const VCS_MARKERS: [&str; 3] = [".git", ".svn", ".hg"];
 
 #[inline(always)]
