025092da3d148fd0bd9f530b4102aab5c5be6413
ReBlast <reblastoff@gmail.com>
2024-11-21 15:23:31 +0500

    refactor(`AssetType`): rename `Lsp` to `LspManager`, allow parsing from a string slice

--- src/util/types.rs
+++ src/util/types.rs
@@ -3,19 +3,19 @@ pub enum AssetType {
     Language,
     FileBrowser,
     PluginManager,
-    Lsp,
+    LspManager,
     Vcs,
 }
 
 impl AssetType {
     #[inline(always)]
-    pub fn from(value: i32) -> Option<AssetType> {
+    pub fn from(value: &str) -> Option<AssetType> {
         match value {
-            0 => Some(AssetType::Language),
-            1 => Some(AssetType::FileBrowser),
-            2 => Some(AssetType::PluginManager),
-            3 => Some(AssetType::Lsp),
-            4 => Some(AssetType::Vcs),
+            "language" => Some(AssetType::Language),
+            "file_browser" => Some(AssetType::FileBrowser),
+            "plugin_manager" => Some(AssetType::PluginManager),
+            "lsp_manager" => Some(AssetType::LspManager),
+            "vcs" => Some(AssetType::Vcs),
             _ => None,
         }
     }
