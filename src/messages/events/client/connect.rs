2e7d5d40a847c2caacc8129d06fbb1cd6e3b837e
ReBlast <reblastoff@gmail.com>
2024-11-22 18:04:01 +0500

    feat(events): handle client connection

--- src/messages/events/client/connect.rs
+++ src/messages/events/client/connect.rs
@@ -1,11 +1,24 @@
 #[derive(Debug, Default)]
 pub struct ConnectEvent;
 
-use crate::messages::events::event::{EventContext, OnEvent};
+use std::sync::atomic::Ordering;
 
+use crate::{
+    ipc::pipe::PipeServerImpl,
+    json::Json,
+    messages::events::{
+        event::{EventContext, OnEvent},
+        server::ReadyEvent,
+    },
+};
 
 impl OnEvent for ConnectEvent {
-    fn on_event(self, _ctx: &EventContext) -> crate::Result<()> {
+    fn on_event(self, ctx: &EventContext) -> crate::Result<()> {
+        if ctx.rich_client.is_ready.load(Ordering::SeqCst) {
+            ctx.pipe
+                .write_to(ctx.client_id, Json::serialize(&ReadyEvent)?.as_bytes())?;
+        }
+
         Ok(())
     }
 }
