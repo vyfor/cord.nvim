eedc94a3efd09724ec5b2581a063c87608a82020
vyfor <reblastoff@gmail.com>
2024-11-29 19:46:11 +0500

    feat: display the most recent activity in case a client disconnects; clear the status if none are connected

--- src/messages/events/client/disconnect.rs
+++ src/messages/events/client/disconnect.rs
@@ -1,11 +1,28 @@
-use crate::messages::events::event::{EventContext, OnEvent};
+use crate::{
+    messages::events::event::{EventContext, OnEvent},
+    presence::types::Packet,
+};
 
 #[derive(Debug, Default)]
 pub struct DisconnectEvent;
 
 impl OnEvent for DisconnectEvent {
     fn on_event(self, ctx: &mut EventContext) -> crate::Result<()> {
-        ctx.cord.session_manager.remove_session(ctx.client_id);
+        let mut sessions = ctx.cord.session_manager.sessions.write().unwrap();
+        sessions.remove(&ctx.client_id);
+        let latest = sessions
+            .iter()
+            .filter(|s| s.1.last_activity.is_some())
+            .max_by_key(|s| s.1.last_updated);
+
+        if let Some((_, session)) = latest {
+            ctx.cord.rich_client.update(&Packet::new(
+                ctx.cord.rich_client.pid,
+                session.last_activity.as_ref(),
+            ))?;
+        } else {
+            ctx.cord.rich_client.clear()?;
+        }
 
         Ok(())
     }
