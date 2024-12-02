f05f12673a962d13efe9fecaa3969c8a40f77fc6
vyfor <reblastoff@gmail.com>
2024-12-02 11:26:40 +0500

    feat: switch to next available activity on `clear_activity` event

--- src/messages/events/client/clear_activity.rs
+++ src/messages/events/client/clear_activity.rs
@@ -1,14 +1,26 @@
-use crate::messages::events::event::{EventContext, OnEvent};
+use crate::{
+    messages::events::event::{EventContext, OnEvent},
+    presence::packet::Packet,
+};
 
 #[derive(Debug, Default)]
 pub struct ClearActivityEvent;
 
 impl OnEvent for ClearActivityEvent {
     fn on_event(self, ctx: &mut EventContext) -> crate::Result<()> {
-        if let Some(mut session) = ctx.cord.session_manager.get_session_mut(ctx.client_id) {
+        let sessions = ctx.cord.session_manager.sessions.read().unwrap();
+        let latest = sessions
+            .iter()
+            .filter(|s| s.1.last_activity.is_some())
+            .max_by_key(|s| s.1.last_updated);
+
+        if let Some((_, session)) = latest {
+            ctx.cord.rich_client.update(&Packet::new(
+                ctx.cord.rich_client.pid,
+                session.last_activity.as_ref(),
+            ))?;
+        } else {
             ctx.cord.rich_client.clear()?;
-            session.last_activity = None;
-            session.last_updated = 0;
         }
 
         Ok(())
