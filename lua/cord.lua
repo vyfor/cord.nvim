6313f45ed0dcb282da54ac508ca3d7d9a710a128
vyfor <reblastoff@gmail.com>
2024-12-02 10:21:58 +0500

    perf(lua): lazily load other modules

--- lua/cord.lua
+++ lua/cord.lua
@@ -1,39 +1,39 @@
-local logger = require 'cord.util.logger'
 local ipc = require 'cord.core.ipc'
-local config = require 'cord.util.config'
-local Handler = require 'cord.event.receiver'
 
 local M = {}
 
 function M.setup(opts)
-  logger.set_level(config.values.log_level)
+  local client = ipc.new { pipe_name = '\\\\.\\pipe\\cord-ipc' }
 
-  local client = ipc.new {
-    pipe_name = '\\\\.\\pipe\\cord-ipc',
-  }
-  local handler = Handler.new(client)
+  client:connect(function()
+    local Handler = require 'cord.event.receiver'
 
-  handler:register('ready', function()
-    local Producer = require 'cord.event.sender'
-    local ActivityManager = require 'cord.activity.manager'
+    M.handler = Handler.new(client)
 
-    logger.info 'Connected to Discord'
+    M.handler:register('ready', function()
+      local config = require 'cord.util.config'
+      local logger = require 'cord.util.logger'
 
-    config:validate(opts or {})
-    if config.values.hooks.on_ready then config.values.hooks.on_ready() end
+      config:validate(opts or {})
+      logger.set_level(config.values.log_level)
+      logger.info 'Connected to Discord'
 
-    M.producer = Producer.new(client)
-    M.producer:initialize(config.values)
-    M.manager = ActivityManager.new { tx = M.producer, config = config.values }
-    M.manager:run_event_loop()
+      local Producer = require 'cord.event.sender'
+      local ActivityManager = require 'cord.activity.manager'
+
+      if config.values.hooks.on_ready then config.values.hooks.on_ready() end
+      M.producer = Producer.new(client)
+      M.producer:initialize(config.values)
+      M.manager =
+        ActivityManager.new { tx = M.producer, config = config.values }
+
+      M.manager:run_event_loop()
+    end)
+
+    M.handler:run()
   end)
 
-  client:connect(function() handler:run() end)
-
   M.client = client
-  M.handler = handler
 end
 
-return {
-  setup = M.setup,
-}
+return M
