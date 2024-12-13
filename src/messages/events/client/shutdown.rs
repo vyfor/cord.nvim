4e9748c58f605819d074495e20e5c9a81fc58372
vyfor <reblastoff@gmail.com>
2024-12-13 18:06:56 +0500

    refactor: handle shutdowns directly

--- src/messages/events/client/shutdown.rs
+++ src/messages/events/client/shutdown.rs
@@ -1,12 +1,11 @@
 use crate::messages::events::event::{EventContext, OnEvent};
-use crate::server_event;
 
 #[derive(Debug, Default)]
 pub struct ShutdownEvent;
 
 impl OnEvent for ShutdownEvent {
     fn on_event(self, ctx: &mut EventContext) -> crate::Result<()> {
-        ctx.cord.tx.send(server_event!(0, Shutdown)).ok();
+        ctx.cord.shutdown();
 
         Ok(())
     }
