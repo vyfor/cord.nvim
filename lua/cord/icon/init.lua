15e83b3c51a77ebcfb5491bc3bec61df9a4bbe3d
vyfor <reblastoff@gmail.com>
2024-12-20 00:51:17 +0500

    feat(icon): expose locals

--- lua/cord/icon/init.lua
+++ lua/cord/icon/init.lua
@@ -1,23 +1,21 @@
+local M = {}
+
 --- Increment this only when an existing icon is modified
 ---
 --- Appended to the end of the asset URL for refetching; otherwise, it will be loaded from the cache, thus not being updated
-local ICONS_VERSION = '3'
-local ICONS_URL = 'https://raw.githubusercontent.com/vyfor/icons/master/icons/'
-local DEFAULT_ICON_STYLE = 'onyx'
+M.ICONS_VERSION = '3'
+M.ICONS_URL = 'https://raw.githubusercontent.com/vyfor/icons/master/icons/'
+M.ICON_STYLE = 'onyx'
 
-local function get(name, style)
-  return ICONS_URL
-    .. (style or DEFAULT_ICON_STYLE)
+M.get = function(name, style)
+  return M.ICONS_URL
+    .. (style or M.ICON_STYLE)
     .. '/'
     .. name
     .. '.png?v='
-    .. ICONS_VERSION
+    .. M.ICONS_VERSION
 end
 
-local function set_style(style) DEFAULT_ICON_STYLE = style end
+M.set_style = function(style) M.ICON_STYLE = style end
 
-return {
-  style = DEFAULT_ICON_STYLE,
-  get = get,
-  set_style = set_style,
-}
+return M
