3092bbce6d7f828b78b12f57ed44287edf73329b
vyfor <reblastoff@gmail.com>
2024-12-20 00:09:52 +0500

    chore: bump assets version

--- lua/cord/icon/init.lua
+++ lua/cord/icon/init.lua
@@ -1,7 +1,7 @@
 --- Increment this only when an existing icon is modified
 ---
 --- Appended to the end of the asset URL for refetching; otherwise, it will be loaded from the cache, thus not being updated
-local ICONS_VERSION = '2'
+local ICONS_VERSION = '3'
 local ICONS_URL = 'https://raw.githubusercontent.com/vyfor/icons/master/icons/'
 local DEFAULT_ICON_STYLE = 'onyx'
 
