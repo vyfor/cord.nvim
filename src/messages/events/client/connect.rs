e4522d61c5510d511868eaf542077387362a1979
vyfor <reblastoff@gmail.com>
2024-12-27 13:14:28 +0500

    refactor: remove unnecessary events

--- src/messages/events/client/connect.rs
+++ src/messages/events/client/connect.rs
@@ -5,16 +5,13 @@ use std::sync::atomic::Ordering;
 
 use crate::ipc::pipe::PipeServerImpl;
 use crate::messages::events::event::{EventContext, OnEvent};
-use crate::messages::events::server::{InitializeEvent, ReadyEvent};
+use crate::messages::events::server::ReadyEvent;
 use crate::protocol::msgpack::MsgPack;
 
 impl OnEvent for ConnectEvent {
     fn on_event(self, ctx: &mut EventContext) -> crate::Result<()> {
-        ctx.cord
-            .pipe
-            .write_to(ctx.client_id, &MsgPack::serialize(&InitializeEvent)?)?;
-
         if ctx.cord.rich_client.is_ready.load(Ordering::SeqCst) {
+            println!("Sending ready event...");
             ctx.cord
                 .pipe
                 .write_to(ctx.client_id, &MsgPack::serialize(&ReadyEvent)?)?;
