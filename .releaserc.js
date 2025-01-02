8149653224aab7a69b21b79970381bbae071f135
vyfor <reblastoff@gmail.com>
2025-01-02 22:23:43 +0500

    chore: update semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -13,8 +13,11 @@ module.exports = {
     [
       "@semantic-release/release-notes-generator",
       {
+        "preset": "conventionalcommits",
         "writerOpts": {
           "headerPartial": "# {{#with context}}{{#if isServerUpdate}}⚙️ {{/if}}{{/with}}[{{currentTag}}]{{#if title}} {{title}}{{/if}}\n\n{{#if date}}_{{date}}_{{/if}}",
+          "commitPartial": "* {{subject}} ([{{hash}}]({{#if @root.repository}}{{@root.repository}}/commit/{{hash}}{{else}}{{@root.repoUrl}}/commit/{{hash}}{{/if}}))\n",
+          "mainTemplate": "{{> header}}\n\n{{#each commitGroups}}\n\n{{#if title}}\n### {{title}}\n\n{{/if}}{{#each commits}}{{> commit}}{{/each}}\n{{/each}}\n\n{{> footer}}",
           "transform": function (commit, context) {
             if (!context.context) context.context = {};
 
@@ -22,9 +25,17 @@ module.exports = {
               context.context.isServerUpdate = true;
             }
 
+            const shortHash = commit.hash.substring(0, 7);
+
             return {
-              title: commit.subject,
-              body: commit.body || ''
+              type: commit.type,
+              scope: commit.scope,
+              subject: commit.subject || '',
+              hash: shortHash,
+              shortDesc: commit.subject || '',
+              body: commit.body || '',
+              footer: commit.footer || '',
+              notes: commit.notes || []
             };
           }
         }
