48cda89f7dc9def8d534258d916b3b2085169e0a
vyfor <reblastoff@gmail.com>
2025-01-03 13:27:08 +0500

    ci: track latest server version with changes in `server-version.txt`

--- .releaserc.js
+++ .releaserc.js
@@ -10,40 +10,7 @@ module.exports = {
   "repositoryUrl": "https://github.com/vyfor/cord.nvim",
   "plugins": [
     "@semantic-release/commit-analyzer",
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        "writerOpts": {
-          "headerPartial": "# {{#with context}}{{#if isServerUpdate}}⚙️ {{/if}}{{/with}}[{{currentTag}}]{{#if title}} {{title}}{{/if}}\n\n{{#if date}}_{{date}}_{{/if}}",
-          "commitPartial": "* {{#if scope}}**{{scope}}**: {{/if}}{{subject}} ([#{{commit.short}}]({{@root.repository}}/commit/{{hash}}))\n",
-          "transform": function (commit, context) {
-            if (!context.context) context.context = {};
-
-            if (commit.scope === 'server') {
-              context.context.isServerUpdate = true;
-            }
-
-            return {};
-          }
-        },
-        "presetConfig": {
-          "types": [
-            { type: 'feat', section: 'Features' },
-            { type: 'feature', section: 'Features' },
-            { type: 'fix', section: 'Bug Fixes' },
-            { type: 'perf', section: 'Performance Improvements' },
-            { type: 'revert', section: 'Reverts' },
-            { type: 'docs', section: 'Documentation', hidden: true },
-            { type: 'style', section: 'Styles', hidden: false },
-            { type: 'chore', section: 'Miscellaneous Chores', hidden: true },
-            { type: 'refactor', section: 'Code Refactoring', hidden: false },
-            { type: 'test', section: 'Tests', hidden: true },
-            { type: 'build', section: 'Build System', hidden: true },
-            { type: 'ci', section: 'Continuous Integration', hidden: true }
-          ]
-        }
-      }
-    ],
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/github",
       {
