97b734e94fa63b0266837062d59368a6ae30fad7
vyfor <reblastoff@gmail.com>
2025-01-07 16:44:02 +0500

    ci: fix `release.yml`

--- .releaserc.js
+++ .releaserc.js
@@ -28,13 +28,11 @@ module.exports = {
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
-    ["@semantic-release-cargo/semantic-release-cargo"],
     [
       "@semantic-release/exec",
       {
-        "verifyConditionsCmd": "./semantic-release-cargo verify-conditions",
-        "prepareCmd": "./semantic-release-cargo prepare ${nextRelease.version}",
-        "publishCmd": "./semantic-release-cargo publish"
+        "verifyConditionsCmd": "semantic-release-cargo verify-conditions",
+        "publishCmd": "semantic-release-cargo publish"
       }
     ]
   ]
