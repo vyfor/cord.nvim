dd5a79c79443d113030abcbdb36d3a8ef94f066b
vyfor <reblastoff@gmail.com>
2025-01-10 17:23:05 +0500

    chore(icons): bump assets version

--- lua/cord/api/icon.lua
+++ lua/cord/api/icon.lua
@@ -3,17 +3,12 @@ local M = {}
 --- Increment this only when an existing icon is modified
 ---
 --- Appended to the end of the asset URL for refetching; otherwise, it will be loaded from the cache, thus not being updated
-M.ICONS_VERSION = '7'
+M.ICONS_VERSION = '8'
 M.ICONS_URL = 'https://raw.githubusercontent.com/vyfor/icons/master/icons/'
 M.ICON_THEME = 'onyx'
 
 M.get = function(name, theme)
-  return M.ICONS_URL
-    .. (theme or M.ICON_THEME)
-    .. '/'
-    .. name
-    .. '.png?v='
-    .. M.ICONS_VERSION
+  return M.ICONS_URL .. (theme or M.ICON_THEME) .. '/' .. name .. '.png?v=' .. M.ICONS_VERSION
 end
 
 M.set_theme = function(theme)
