1bf1e4096376cfc58f678f5b81a8e3039dcc2e02
vyfor <vyforium@gmail.com>
2025-03-02 21:35:47 +0500

    chore: switch to romver
    closes #212

--- .releaserc.js
+++ .releaserc.js
@@ -9,8 +9,27 @@ module.exports = {
   ],
   "repositoryUrl": "https://github.com/vyfor/cord.nvim",
   "plugins": [
-    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "preset": "angular",
+        "releaseRules": [
+          { "scope": "no-release", "release": false },
+          { "type": "docs", "release": false },
+          { "type": "refactor", "release": false },
+          { "type": "style", "release": false },
+          { "type": "test", "release": false },
+          { "type": "chore", "release": false },
+          { "type": "build", "release": false },
+          { "type": "perf", "release": "patch" },
+          { "type": "fix", "release": "patch" },
+          { "type": "feat", "release": "patch" },
+          { breaking: true, release: "minor" },
+          { "type": "release", "scope": "major", "release": "major" }
+        ]
+      }
+    ],
     [
       "@semantic-release/github",
       {
