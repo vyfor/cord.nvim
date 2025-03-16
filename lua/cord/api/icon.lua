658951c02f2f7945297e344b233090224afdabae
vyfor <vyforium@gmail.com>
2025-03-16 17:46:51 +0500

    feat(config): validate theme and flavor names

--- lua/cord/api/icon.lua
+++ lua/cord/api/icon.lua
@@ -8,6 +8,9 @@ M.ICONS_URL = 'https://raw.githubusercontent.com/vyfor/icons/master/icons/'
 M.ICON_THEME = 'default'
 M.THEME_FLAVOR = 'dark'
 
+M.supported_themes = { 'default', 'atom', 'catppuccin' }
+M.supported_flavors = { 'dark', 'light', 'accent' }
+
 M.get = function(name, theme, flavor)
   return M.ICONS_URL
     .. (theme or M.ICON_THEME)
@@ -20,6 +23,14 @@ M.get = function(name, theme, flavor)
 end
 
 M.set = function(theme, flavor)
+  if not vim.tbl_contains(M.supported_themes, theme) then
+    require('cord.plugin.log').log_raw(vim.log.levels.WARN, 'Unknown theme: ' .. theme)
+  end
+
+  if not vim.tbl_contains(M.supported_flavors, flavor) then
+    require('cord.plugin.log').log_raw(vim.log.levels.WARN, 'Unknown flavor: ' .. flavor)
+  end
+
   M.ICON_THEME = theme
   M.THEME_FLAVOR = flavor
   M.DEFAULT_IDLE_ICON = (theme == 'default' and flavor ~= 'accent') and 'keyboard' or 'idle'
