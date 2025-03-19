3c520f1b94931d66adab9cb1df59d066f50e9820
vyfor <vyforium@gmail.com>
2025-03-19 17:20:28 +0500

    chore: remove `CHANGELOG.md`

--- .releaserc.js
+++ .releaserc.js
@@ -41,9 +41,6 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        "assets": [
-          "CHANGELOG.md"
-        ],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
