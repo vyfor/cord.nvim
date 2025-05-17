4f1238774eeef88f96120b109831de39cb3daad7
vyfor <vyforium@gmail.com>
2025-05-17 15:20:02 +0500

    feat: add support for manual plugin startup

--- lua/cord.lua
+++ lua/cord.lua
@@ -4,6 +4,24 @@ M = {
   ---@param opts? CordConfig Configuration options for Cord
   setup = function(opts)
     if opts then M.user_config = opts end
+
+    if vim.g.cord_defer_startup == true then
+      local config = require('cord.plugin.config.util').validate(opts)
+      if not config then return end
+
+      if config.enabled then
+        vim.cmd [[
+            augroup Cord
+                autocmd!
+                autocmd VimLeavePre * lua require 'cord.server':cleanup()
+            augroup END
+
+            command! -nargs=+ -complete=customlist,CordCompleteList Cord lua require'cord.api.command'.handle('<q-args>')
+        ]]
+
+        require('cord.server'):initialize()
+      end
+    end
   end,
 }
 
