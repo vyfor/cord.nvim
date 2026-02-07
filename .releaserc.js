1a20523dd2618488c3b3df696c7d5dc46f38f71b
vyfor <vyfor@proton.me>
2026-02-07 14:09:30 +0500

    ci(release): fix change detection

--- .releaserc.js
+++ .releaserc.js
@@ -40,15 +40,11 @@ module.exports = {
           PREVIOUS_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "")
           if [ -n "$PREVIOUS_TAG" ] && git diff --quiet "$PREVIOUS_TAG" HEAD -- src/; then
             echo "skipping metadata update"
-            if [ -f .github/server-metadata.txt ]; then
-              OLD_TS=$(cut -d'|' -f2 .github/server-metadata.txt)
-              echo "\${nextRelease.version}|$OLD_TS" > .github/server-metadata.txt
-              echo "new metadata: \${nextRelease.version}|$OLD_TS"
-            fi
-          else
-            echo "updating server metadata"
-            echo "\${nextRelease.version}|\${process.env.RELEASE_TIMESTAMP}" > .github/server-metadata.txt
+            exit 0
           fi
+
+          echo "updating server metadata"
+          echo "\${nextRelease.version}|\${process.env.RELEASE_TIMESTAMP}" > .github/server-metadata.txt
         `,
         "publishCmd": "semantic-release-cargo publish"
       }
