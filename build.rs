0b5ca0a233634862d92b04cf31d8f36386a45d6c
vyfor <vyfor@proton.me>
2026-07-14 14:30:54 +0500

    fix(ci): fix release workflow

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
 fn main() {
-    let content = std::fs::read_to_string(".github/server-metadata.txt").unwrap();
-    let version = content.split('|').next().unwrap_or(&content).trim();
+    let version = std::env::var("CORD_VERSION").unwrap_or_else(|_| {
+        let content = std::fs::read_to_string(".github/server-metadata.txt")
+            .expect("server-metadata.txt not found");
+        content
+            .split('|')
+            .next()
+            .unwrap_or(&content)
+            .trim()
+            .to_string()
+    });
     println!("cargo:rustc-env=CORD_VERSION={}", version);
 }
