6028cb612798cf9c7ea6e7835c930e7e700064d1
vyfor <vyfor@proton.me>
2026-07-18 16:04:29 +0500

    feat(log): allow overriding the log level via `CORD_LOG_LEVEL` env var

--- lua/cord/api/log/init.lua
+++ lua/cord/api/log/init.lua
@@ -1,7 +1,22 @@
 local logfile = os.getenv 'CORD_LOG_FILE'
 
+local module
 if logfile and logfile ~= '' then
-  return require 'cord.api.log.file'
+  module = require 'cord.api.log.file'
 else
-  return require 'cord.api.log.notify'
+  module = require 'cord.api.log.notify'
 end
+
+local env_level = os.getenv 'CORD_LOG_LEVEL'
+if env_level and env_level ~= '' then
+  local level = tonumber(env_level)
+  if not level then
+    level = vim.log.levels[string.upper(env_level)]
+  end
+  if level then
+    module.set_level(level)
+    module.set_level = function() end
+  end
+end
+
+return module
